4917b158dadd3a71252d78b4cdef14ae9a64955d
Kaiser Mighty <unikmey@gmail.com>
2024-04-06 14:50:20 -0700

    Refactored JS

--- script.js
+++ script.js
@@ -1,131 +1,107 @@
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function ()
-{
-    if (this.readyState == 4 && this.status == 200)
-    {
-        routeDetails(this);
-    }
-};
+// Initial function/main, show routes
+loadXML(1,"https://retro.umoiq.com/service/publicXMLFeed?command=routeList&a=sfmuni-sandbox");
 
-xmlhttp.open("GET", "https://retro.umoiq.com/service/publicXMLFeed?command=routeList&a=sfmuni-sandbox", true);
-xmlhttp.send();
-
-function loadRoutes()
+// Fetch all XML file and redirect to next function
+function loadXML(nextFunc, link)
 {
     let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function ()
     {
         if (this.readyState == 4 && this.status == 200)
         {
-            routeDetails(this);
+            let xmlDoc = this.responseXML;
+            // Redirect to next function
+            switch (nextFunc)
+            {
+                case 1: // Routes
+                    routeDetails(xmlDoc);
+                    break;
+                case 2: // Stops
+                    stopDetails(xmlDoc);
+                    break;
+                case 3: // Times
+                    busDetails(xmlDoc);
+            }
         }
     };
-
-    xmlhttp.open("GET", "https://retro.umoiq.com/service/publicXMLFeed?command=routeList&a=sfmuni-sandbox", true);
+    xmlhttp.open("GET", link, true);
     xmlhttp.send();
 }
 
-function loadStops(x)
-{
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function ()
-    {
-        if (this.readyState == 4 && this.status == 200)
-        {
-            stopDetails(this);
-        }
-    };
-
-    xmlhttp.open("GET", "https://retro.umoiq.com/service/publicXMLFeed?command=routeConfig&a=sfmuni-sandbox&r="+x, true);
-    xmlhttp.send();
-}
-
-function loadBuses(x,y)
-{
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function ()
-    {
-        if (this.readyState == 4 && this.status == 200)
-        {
-            busDetails(this);
-        }
-    };
-
-    xmlhttp.open("GET", "https://retro.umoiq.com/service/publicXMLFeed?command=predictions&a=sfmuni-sandbox&stopId="+x+"&routeTag="+y, true);
-    xmlhttp.send();
-}
-
+// Parse and display when the next bus is arriving
 function busDetails(xml)
 {
-    let i;
-    let xmlDoc = xml.responseXML;
-    let button =
-        `<h2>Next Bus Arrives in</h2>`;
-    let x = xmlDoc.getElementsByTagName("prediction");
-    let m = xmlDoc.getElementsByTagName("predictions");
-    let n = m[0].getAttribute("routeTag")
-    let y = 3;
-    if (x.length < 3) y = x.length;
+    let times = xml.getElementsByTagName("prediction");
+    let stopInfo = xml.getElementsByTagName("predictions");
+    let route = stopInfo[0].getAttribute("routeTag");
     
-    button += "<button type='button' class='button' onclick='loadStops(\""+n+"\")'> Back </button><br>";
-    for (i = 0; i < y; i++)
+    // Always only display 3 or less times
+    let displayAmount = 3;
+    if (times.length < 3) displayAmount = times.length;
+
+    // Create Back Button
+    let HTML = `<h2 class='choice_header'>Next Bus Arrives in</h2>`;
+    let link = "https://retro.umoiq.com/service/publicXMLFeed?command=routeConfig&a=sfmuni-sandbox&r="+route;
+    HTML += "<button type='button' class='back_button' onclick='loadXML(2,\""+link+"\")'>Back</button><br>";
+    
+    // Iterate through times and display them
+    for (let i = 0; i < displayAmount; i++)
     {
-        if (x[i].getAttribute("minutes") == 0)
+        // Display as seconds
+        if (times[i].getAttribute("minutes") == 0)
         {
-            button += "<h3>" + x[i].getAttribute("seconds") + " seconds" + "</h3>";
+            HTML += "<h3 class='time_entry'>" + times[i].getAttribute("seconds") + " seconds" + "</h3>";
         }
-        else if (x[i].getAttribute("minutes") == 1)
+        // Display as minute
+        else if (times[i].getAttribute("minutes") == 1)
         {
-            button += "<h3>" + x[i].getAttribute("minutes") + " minute" + "</h3>";
+            HTML += "<h3 class='time_entry'>" + times[i].getAttribute("minutes") + " minute" + "</h3>";
         }
+        // Display as minutes
         else
         {
-            button += "<h3>" + x[i].getAttribute("minutes") + " minutes" + "</h3>";
+            HTML += "<h3 class='time_entry'>" + times[i].getAttribute("minutes") + " minutes" + "</h3>";
         }
     }
-    let j = m[0].getAttribute("stopTag") + " " + m[0].getAttribute("routeTag") + " " + m[0].getAttribute("routeTag") + " " + m[0].getAttribute("stopTitle");
-    button += "<button type='button' class='button' onclick='favorite(\""+j+"\")'>Favorite Stop</button>";
-    document.getElementById("id").innerHTML = button;
+    document.getElementById("id").innerHTML = HTML;
 }
 
+// Parse and display all stops for chosen route
 function stopDetails(xml)
 {
-    let i;
-    let xmlDoc = xml.responseXML;
-    let button =
-        `<h2>Choose your Stop</h2>`;
-    let x = xmlDoc.getElementsByTagName("stop");
-    let z = xmlDoc.getElementsByTagName("route")[0].getAttribute("tag");
+    let stops = xml.getElementsByTagName("stop");
+    let route = xml.getElementsByTagName("route")[0].getAttribute("tag");
     
-    button += "<button type='button' class='button' onclick='loadRoutes()'> Back </button><br>";
-    for (i = 0; i < x.length; i++)
+    // Create Back Button
+    let HTML = `<h2 class='choice_header'>Choose your Stop</h2>`;
+    let link = "https://retro.umoiq.com/service/publicXMLFeed?command=routeList&a=sfmuni-sandbox";
+    HTML += "<button type='button' class='back_button' onclick='loadXML(1,\""+link+"\")'>Back</button><br>";
+    
+    // Iterate through stops and display them
+    for (let i = 0; i < stops.length; i++)
     {
-        let y = x[i].getAttribute("stopId");
-        let thing = x[i].getAttribute("title");
-        if (thing == null || y == null) continue;
-        button += "<button type='button' class='button' onclick='loadBuses(\""+y+"\",\""+z+"\")'>"+thing+"</button>";
+        let stop = stops[i].getAttribute("stopId");
+        let title = stops[i].getAttribute("title");
+        if (title == null || route == null) continue; // Skip invalid entries
+        link = "https://retro.umoiq.com/service/publicXMLFeed?command=predictions&a=sfmuni-sandbox&stopId="+stop+"&routeTag="+route
+        HTML += "<button type='button' class='button' onclick='loadXML(3,\""+link+"\")'>"+title+"</button>";
     }
-    document.getElementById("id").innerHTML = button;
+    document.getElementById("id").innerHTML = HTML;
 }
 
+// Parse and display all routes
 function routeDetails(xml)
 {
-    let i;
-    let xmlDoc = xml.responseXML;
-    let button =
-        `<h2>Choose your Route</h2>`;
-    let x = xmlDoc.getElementsByTagName("route");
+    let HTML = `<h2 class='choice_header'>Choose your Route</h2>`;
+    let routes = xml.getElementsByTagName("route");
 
-    for (i = 0; i < x.length; i++)
+    // Iterate through all routes and display them
+    for (let i = 0; i < routes.length; i++)
     {
-        let y = x[i].getAttribute("tag");
-        let thing = x[i].getAttribute("title");
-        button += "<button type='button' class='button' onclick='loadStops(\""+y+"\")'>"+thing+"</button>";
+        let route = routes[i].getAttribute("tag");
+        let title = routes[i].getAttribute("title");
+        let link = "https://retro.umoiq.com/service/publicXMLFeed?command=routeConfig&a=sfmuni-sandbox&r="+route;
+        HTML += "<button type='button' class='button' onclick='loadXML(2,\""+link+"\")'>"+title+"</button>";
     }
-    document.getElementById("id").innerHTML = button;
-}
-
-function favorite(x)
-{
-    
+    document.getElementById("id").innerHTML = HTML;
 }
